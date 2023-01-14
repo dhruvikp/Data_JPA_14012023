4d63bf4f6232ebdd6bf357ef08c591cc7f5d74ce
Dhruvik <dhruvik@simplilearn.com>
2023-01-14 12:39:53 +0530

    Added changes

---
+++ src/test/java/com/simplilearn/Data_JPA_14012023/DataJpa14012023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Data_JPA_14012023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpa14012023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
